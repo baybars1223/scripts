6f9bce9352cbdef54bd11f9d8353bc068adda5ec
Michael Thomasson <mjthomasson@gmail.com>
2018-10-10 15:52:39 -0500

    add rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -4,4 +4,4 @@ else
   echo "Restarting container.."
   docker stop baybars1223/sysarcio:latest && docker rm baybars1223/sysarcio:latest
 fi
-docker pull baybars1223/sysarcio:latest . && docker run --name baybars1223/sysarcio:latest -p 80:80 --env-file .env -d baybars1223/sysarcio:latest
+docker pull baybars1223/sysarcio:latest && docker run --name baybars1223/sysarcio:latest -p 80:80 --env-file .env -d baybars1223/sysarcio:latest
