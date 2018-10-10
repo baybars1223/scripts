87c3b7cd1f88babd5b1dd09979c9c25e5301643d
Michael Thomasson <mjthomasson@gmail.com>
2018-10-10 16:43:51 -0500

    add env to run

--- rebuild.sh
+++ rebuild.sh
@@ -4,4 +4,4 @@ else
   echo "Restarting container.."
   docker stop baybars1223/sysarcio:latest && docker rm baybars1223/sysarcio:latest
 fi
-docker pull baybars1223/sysarcio:latest && docker run -p 80:80 baybars1223/sysarcio:latest
+docker pull baybars1223/sysarcio:latest && docker run --env GRAPHENEDB_PASSWORD:$GRAPHENEDB_PASSWORD --env GRAPHENEDB_URI:$GRAPHENEDB_URI	--env GRAPHENEDB_USERNAME:$GRAPHENEDB_USERNAME -p 80:80 baybars1223/sysarcio:latest
