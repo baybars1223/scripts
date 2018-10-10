d9939233fabea930da9a74e06aedf08fd539b427
Michael Thomasson <mjthomasson@gmail.com>
2018-10-10 15:06:16 -0500

    add rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,7 @@
+if [ "$1" == "nocontainer" ]; then
+  echo "Starting new container.."
+else
+  echo "Restarting container.."
+  docker stop baybars1223/sysarcio:latest && docker rm baybars1223/sysarcio:latest
+fi
+docker pull baybars1223/sysarcio:latest . && docker run --name baybars1223/sysarcio:latest -p 80:80 --env-file .env -d baybars1223/sysarcio:latest
