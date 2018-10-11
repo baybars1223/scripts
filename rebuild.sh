17b356da652a3aaf559adf9875317c82366adc52
Michael Thomasson <mjthomasson@gmail.com>
2018-10-10 20:21:05 -0500

    i win????

--- rebuild.sh
+++ rebuild.sh
@@ -4,4 +4,4 @@ else
   echo "Restarting container.."
   docker stop baybars1223/sysarcio:latest && docker rm baybars1223/sysarcio:latest  
 fi
-ls -all && cat .env && docker pull baybars1223/sysarcio:latest && docker run --env-file .env -p 80:80 baybars1223/sysarcio:latest
+ls -all && cat .env && docker pull baybars1223/sysarcio:latest && docker run --env-file .env -p 80:80 -d baybars1223/sysarcio:latest
