d20ea6416472a207e2516795dc795622ba3573b4
Michael Thomasson <mjthomasson@gmail.com>
2018-10-10 20:16:39 -0500

    i win???

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,6 @@ if [ "$1" == "nocontainer" ]; then
   echo "Starting new container.."
 else
   echo "Restarting container.."
-  docker stop baybars1223/sysarcio:latest && docker rm baybars1223/sysarcio:latest && ls -all && cat .env
+  docker stop baybars1223/sysarcio:latest && docker rm baybars1223/sysarcio:latest  
 fi
-docker pull baybars1223/sysarcio:latest && docker run --env-file .env -p 80:80 baybars1223/sysarcio:latest
+ls -all && cat .env && docker pull baybars1223/sysarcio:latest && docker run --env-file .env -p 80:80 baybars1223/sysarcio:latest
