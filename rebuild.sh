ef27c19e10ba7b967dd87f3016d0b698525d0031
Michael Thomasson <mjthomasson@gmail.com>
2018-10-11 15:31:25 -0500

    gave container name to more easily remove also add prune

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,6 @@ if [ "$1" == "nocontainer" ]; then
   echo "Starting new container.."
 else
   echo "Restarting container.."
-  docker stop baybars1223/sysarcio:latest && docker rm baybars1223/sysarcio:latest  
+  docker stop sysarcioServer && docker rm sysarcioServer && docker image prune && docker system prune -a -f
 fi
-ls -all && cat .env && docker pull baybars1223/sysarcio:latest && docker run --env-file .env -p 80:80 -d baybars1223/sysarcio:latest
+ls -all && cat .env && docker pull baybars1223/sysarcio:latest && docker run --env-file .env -p 80:80 --name sysarcioServer -d baybars1223/sysarcio:latest
