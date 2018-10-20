d0ca59fea7f313b8bac0d32ecd09d3d2f8a90b6f
Michael Thomasson <mjthomasson@gmail.com>
2018-10-20 14:38:18 -0500

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,8 @@
+
 if [ "$1" == "nocontainer" ]; then
   echo "Starting new container.."
 else
   echo "Restarting container.."
   docker stop sysarcioServer && docker rm sysarcioServer && docker image prune && docker system prune -a -f
 fi
-ls -all && cat .env && docker pull baybars1223/sysarcio:latest && docker run --env-file .env -p 80:80 --name sysarcioServer -d --network=sysarcio baybars1223/sysarcio:latest
+ls -all && cat .env && docker pull baybars1223/sysarcio:latest && docker run --env-file .env -p 80:80 --name sysarcioServer baybars1223/sysarcio:latest
