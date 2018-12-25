33c8292e400cdc9d479deecc002a65173a12340c
Michael Thomasson <mjthomasson@gmail.com>
2018-12-25 12:53:07 -0700

    validate input for Twitch viewer counter

--- Browser-User-Scripts/TwitchBrowserTools.js
+++ Browser-User-Scripts/TwitchBrowserTools.js
@@ -2,20 +2,36 @@ const countCategoryViewers = () => {
   let sumViewers = 0;
   const nodes = document.querySelectorAll("div.tw-absolute.tw-bottom-0.tw-left-0.tw-mg-x-05.tw-mg-y-05");
   const scrollArea = document.querySelectorAll("div.tw-flex.tw-flex-column.tw-flex-nowrap.tw-full-height div main")[0].children[0].children[2];
-  const numberOfScrollAttempts = window.prompt("How many times should we try to scroll down?");
-  for (let i = 1; i <= numberOfScrollAttempts; i += 1) {
+  let inputSuccessful = scrollToLoad(scrollArea);
+  if (inputSuccessful.status === true) {
     setTimeout(e => {
-      console.log('scroll #', i);
-      scrollArea.scrollTo(0, 1000000);
-    }, 750 * i);
-  };
-  setTimeout(e => {
-    nodes.forEach(e => {
-      sumViewers += Number(e.children[0].children[0].innerText.split(" ")[0].replace(",",""))
-    });
-    console.log(`We counted ${sumViewers} total viewers`);
-  }, (750 * numberOfScrollAttempts) + 5000);
+      nodes.forEach(e => {
+        sumViewers += Number(e.children[0].children[0].innerText.split(" ")[0].replace(",",""))
+      });
+      console.log(`We counted ${sumViewers} total viewers`);
+    }, (750 * inputSuccessful.returned) + 5000);
+  } else {
+    console.log(inputSuccessful.returned);
+  }
 };
 
+const scrollToLoad = (scrollElement) => {
+  let numberOfScrollAttempts = window.prompt("How many times should we try to scroll down?");
+  if (
+    typeof numberOfScrollAttempts === "number" || 
+    (typeof Number(numberOfScrollAttempts.replace(",","")) === "number" && isNaN(Number(numberOfScrollAttempts.replace(",",""))) !== true)
+  ) {
+    for (let i = 1; i <= numberOfScrollAttempts; i += 1) {
+      setTimeout(() => {
+        console.log('scroll #', i);
+        scrollElement.scrollTo(0, 1000000);
+      }, 750 * i);
+    };
+    return {status: true, returned: numberOfScrollAttempts};
+  } else {
+    window.alert("Invalid entry, try again with a number.");
+    return {status: false, returned: `Could not scroll to load more data. Invalid entry for number of scroll attempts. Input was ${numberOfScrollAttempts}`};
+  }
+}
 
 
